d36d455a16f1478f844b95b59ff7a14cee059241
boevoyogurec <pvoit2006@gmail.com>
2025-04-10 19:21:11 +0300

    добавил хендлеры и команды

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,26 +1,33 @@
-__all__ = [
-    'register_message_handlers'
-]
-
-
-# TODO - Опишите вызов функций обработчиков через маршрутизацию
-# Работа c Router - https://docs.aiogram.dev/en/v3.14.0/dispatcher/router.html
-# Пример работы с Router через декораторы @router - https://mastergroosha.github.io/aiogram-3-guide/routers/
-# Пример работы с Router через функцию сборщик https://stackoverflow.com/questions/77809738/how-to-connect-a-router-in-aiogram-3-x-x#:~:text=1-,Answer,-Sorted%20by%3A
-
-
 from aiogram import types, Router
 from aiogram.filters import Command
 from .keyboard import keyboard  # импорт из клавиатур
 from .callbacks import callback_message  # импорт из коллбека
 
+# Создаем экземпляр Router
+router = Router()
 
 async def process_start_command(message: types.Message):
     '''Команда start'''
     await message.answer(text="Привет!")
 
+async def process_help_command(message: types.Message):
+    '''Команда help'''
+    await message.answer(text="Помощь: доступные команды - /start, /help, /status")
+
+async def process_status_command(message: types.Message):
+    '''Команда status'''
+    await message.answer(text="Статус: бот работает и готов к взаимодействию!")
 
 # Здесь описывается маршрутизация
 def register_message_handlers():
     '''Маршрутизация обработчиков'''
-    pass
+    router.message.register(process_start_command, Command("start"))
+    router.message.register(process_help_command, Command("help"))
+    router.message.register(process_status_command, Command("status"))
+
+    return router  # Возвращаем маршрутизатор для использования в основном файле
+
+# В конце файла можно добавить строку для экспорта функций
+all = [
+    'register_message_handlers'
+]
