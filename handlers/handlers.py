1879767f7a26c9d99ddf297c4bc1f1060d998e72
tkdanil <marvelsuper654@gmail.com>
2025-04-21 15:53:53 +0300

    испраление

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,33 +1,21 @@
+__all__=[
+    "router",
+]
 from aiogram import types, Router
 from aiogram.filters import Command
-from .keyboard import keyboard  # импорт из клавиатур
-from .callbacks import callback_message  # импорт из коллбека
+from .keyboard import main_keyboard
 
 # Создаем экземпляр Router
 router = Router()
 
-async def process_start_command(message: types.Message):
-    '''Команда start'''
-    await message.answer(text="Привет!")
 
-async def process_help_command(message: types.Message):
-    '''Команда help'''
-    await message.answer(text="Помощь: доступные команды - /start, /help, /status")
 
-async def process_status_command(message: types.Message):
-    '''Команда status'''
-    await message.answer(text="Статус: бот работает и готов к взаимодействию!")
 
-# Здесь описывается маршрутизация
-def register_message_handlers():
-    '''Маршрутизация обработчиков'''
-    router.message.register(process_start_command, Command("start"))
-    router.message.register(process_help_command, Command("help"))
-    router.message.register(process_status_command, Command("status"))
+@router.message(Command('help'))
+async def process_start_command(message):
+    await message.answer("хелп!",reply_markup=main_keyboard)
 
-    return router  # Возвращаем маршрутизатор для использования в основном файле
 
-# В конце файла можно добавить строку для экспорта функций
-all = [
-    'register_message_handlers'
-]
+@router.message(Command(commands=["start",'status']))
+async def process_start_command(message):
+    await message.reply(f"{message.from_user.id},{message.from_user.username}")
--- main.py
+++ main.py
@@ -2,28 +2,21 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
-from handlers import register_message_handlers, set_my_commands
+from handlers import router
 
 
 async def main():
-    """
-    Основная функция для установки конфигурации бота.
-    Для создания бота необходимо получить token в telegram https://t.me/BotFather
-    и добавить полученный токен в файл .env
-    """
 
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    # Здесь функция для вызова хендлеров из handlers.py
-    register_message_handlers()
+#Опредление для диспечера маршрутизация из handlers
+    dp.include_router(router)
 
-    # Здесь вызов меню с командами бота
-    set_my_commands
+
 
     # Запуск бота в polling-режиме
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
