d6908059c5d539745569ae25ec61706b44fbe32b
tkdanil <marvelsuper654@gmail.com>
2025-04-24 02:41:25 +0300

    не смог замержить main.py пришлось вручную исправить, остально все перенеслось

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
+# version0.4.0
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
 from config import TOKEN
+from handlers import router
+from utils import setup_logger
 
-#Экземпляр бота и диспетчера
-bot = Bot(token="TOKEN")
-dp = Dispatcher()
-
-#Бот принимает команды, напрмер /start.
-#Создание хенлер - обработчик сообщений, и будем возвращать сообщение
-@dp.message(Command('start'))
-async def process_start_command(message):
-    await message.answer("Hi!")
-
-
-@dp.message()
-async def echo_message(message):
-    await message.answer(message.text)
-
-#фунция запуска проекта
 async def main():
-    await dp.start_polling(bot)
 
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    setup_logger(fname=__name__)
+#Опредление для диспечера маршрутизация из handlers
+    dp.include_router(router)
+
+
+
+    # Запуск бота в polling-режиме
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# Сделал комит
