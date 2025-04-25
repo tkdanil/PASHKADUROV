62d612ed648995e5bddf1f4ce7b5412d7f2fa4f6
tkdanil <marvelsuper654@gmail.com>
2025-04-25 04:39:51 +0300

    create 1 and 2 test

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -12,10 +12,14 @@ router = Router()
 
 
 @router.message(Command('help'))
-async def process_start_command(message):
-    await message.answer("хелп!",reply_markup=main_keyboard)
+async def process_help_command(message):
+    await message.answer(text="хелп!",reply_markup=main_keyboard)
 
 
 @router.message(Command(commands=["start",'status']))
 async def process_start_command(message):
     await message.reply(f"{message.from_user.id},{message.from_user.username}")
+
+@router.message(Command('test'))
+async def process_test_command(message: types.Message):
+    await message.answer(text="test!")
