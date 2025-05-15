42bf9345555df5c2656c9928b05aa847fbb84b98
tkdanil <marvelsuper654@gmail.com>
2025-05-15 21:54:57 +0300

    исправил ошибку

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -8,9 +8,9 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 button_continue = InlineKeyboardButton(text="ДАлее",callback_data="button_continue")
 
-button_tutor = InlineKeyboardButton(text="Слушатель",callback_data="button_tutor")
+button_tutor = InlineKeyboardButton(text="Слушатель",callback_data="button_student")
 
-button_student = InlineKeyboardButton(text="Преподаватель",callback_data="button_student")
+button_student = InlineKeyboardButton(text="Преподаватель",callback_data="button_tutor")
 
 
 
